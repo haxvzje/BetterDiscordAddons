7db7468caa596c49fb8a81ed7fad9c099f974306
Baziki <thaikn1514@gmail.com>
2021-10-14 22:43:34 +0700

    Create NitroStream.plugin.js
    Create NitroStream plugin

---
+++ NitroStream/NitroStream.plugin.js
@@ -0,0 +1,20 @@
+/**
+ * @name NitroStream
+ * @version 1.0.0
+ * @description Allow you stream hq !
+ * @author haxvzje#0901
+ * @authorId 787936379199291392
+ * @website https://hatomagi.website/
+ * @source https://github.com/haxvzje/BetterDiscordAddons/NitroStream
+ */
+
+
+
+ module.exports = class NitroStream{
+   load() {}
+   start() {
+      BdApi.alert("Nitro Stream", "Author: haxvzje, infAura\n\nHope u enjoy !");
+      BdApi.findModuleByProps("getCurrentUser").getCurrentUser().premiumType = 2;
+   }
+   stop(){}
+}
